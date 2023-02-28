b86e10c5fee5bf03963b414d5a11afb3c07f771a
salem ben el cadi <salem.benelcadi@nttdata.com>
2023-02-28 20:26:10 +0100

    create a basic chat UI

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, Request, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -8,3 +11,7 @@ async def websocket_endpoint(websocket: WebSocket):
     while True:
         data = await websocket.receive_text()
         await websocket.send_text(f"You said: {data}")
+
+@app.get("/")
+async def get(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
