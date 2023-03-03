ea3c813c21a67c80a9eed3e6e327f3b68fd1ac1c
salem ben el cadi <salem.benelcadi@nttdata.com>
2023-03-03 12:16:06 +0100

    adding a beta version of a chat app with registration page

--- My_chat_app/app.py
+++ My_chat_app/app.py
@@ -1,26 +1,67 @@
-from fastapi import FastAPI, WebSocket, Request, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from uuid import uuid4
+import csv
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 connected_clients = {}
 
-@app.websocket("/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: str):
-    await websocket.accept()
-    connected_clients[client_id] = websocket
-    try:
-        while True:
-            data = await websocket.receive_text()
-            for id, client in connected_clients.items():
-                if id != client_id:
-                    await client.send_text(f"{client_id}: {data}")
-    except WebSocketDisconnect:
-        del connected_clients[client_id]
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
 
 @app.get("/")
 async def get(request: Request):
-    client_id = str(uuid4())
-    return templates.TemplateResponse("index.html", {"request": request, "client_id": client_id})
+    return templates.TemplateResponse("register.html", {"request": request})
+
+# @app.get("/")
+# async def get(request: Request):
+#     return templates.TemplateResponse("getname.html", {"request": request})
+
+@app.post("/chat")
+async def chat(request: Request, name: str = Form(...)):
+    # with open('client_data.csv', mode='a') as csv_file:
+    #     fieldnames = ['name', 'userAgent', 'language', 'platform', 'ipAddress', 'screenWidth', 'screenHeight', 'windowWidth', 'windowHeight']
+    #     writer = csv.DictWriter(csv_file, fieldnames=fieldnames, delimiter='|')
+    #     if csv_file.tell() == 0:
+    #         writer.writeheader()
+    #     if clientInfo["ipAddress"] == "":
+    #         clientInfo["ipAddress"] = request.client.host
+    #     writer.writerow(clientInfo)
+    # this = templates.TemplateResponse("chat.html", {"request": request, "name": clientInfo["name"]})
+    # name = clientinfo["name"]
+
+    return templates.TemplateResponse("chat.html", {"request": request, "name": name })
+    
+
+@app.websocket("/chat/{name}")
+async def websocket_endpoint(websocket: WebSocket, name: str):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(f"{name}: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"{name} left the chat")
+
+
+
+# @app.post("/hello")
+# async def register(request: Request, name: str = Form(...)):
+#     return templates.TemplateResponse("helloname.html", {"request": request, "name": name})
